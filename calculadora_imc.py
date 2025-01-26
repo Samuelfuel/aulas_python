125566cdcf13ccbab9d5b22b122729699a91faca
Samuel Carneiro <samuelfuel.sf1@gmail.com>
2025-01-26 11:09:48 -0800

    Update calculadora_imc.py

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -8,10 +8,14 @@ imprimir
 
 '''
 
-print("Calculadora IMC")
+print("Calculadora IMC") 
 
-peso=float(input("Digite seu peso em kilos: \n"))
-altura=float(input("Digite sua altura em metros:\n"))
-imc=peso/(altura**2)
+peso=float(input("Digite seu peso em kilos: \n")) #entrada do peso
+
+altura=float(input("Digite sua altura em metros:\n")) #entrada da altura
+
+imc=peso/(altura**2) #calculo do imc
+
 print(f"IMC: {imc:.2f}")
-print("IMC: "+ str(round(imc,2)))
+
+print("IMC: "+ str(round(imc,2))) #impressao do imc (resultado)
